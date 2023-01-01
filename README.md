809f4ced140a9a72e83d74f875bf9d4456ff3c5d
shamskamar <kosimovshamsqamar@gmail.com>
2023-01-02 05:02:34 +0900

    have added the readme file

---
+++ README.md
@@ -0,0 +1,6 @@
+KOREAN UZBEK vocabulary quiz makes
+
+this is the project that reads the pdf file and gets the words in korean and make a simple quiz with all those words
+
+so this project uses the PyPDF2 library
+and this project is in its early stage of development email all your suggestions directly to kosimovshamsqamar@gmail.com 
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # this is the explanation of the project    
 from PyPDF2 import PdfReader
-import time
+# we can import the following classes from the PyPDF library
+# the first is the PdfReader class
+# the second is the PdfWriter class
+# the third is the PdfMerger class 
+
 # there is the method called 'the page'.extract_text() which extracts the text from the page 
 
 
 reader = PdfReader('vocabulary.pdf')
 numberOfPages = len(reader.pages)
 for i in range(numberOfPages):
-    page = reader.pages[i]
     text = page.extract_text()
-    print(text)
-    time.sleep(2)
+    page = reader.pages[i]
+    # so this is the point where we are getting the page extracted as a text
+    # i think we need another for loop to extract the information from the page we gotytujtyujopijaerfg9oi
     
 
 print(numberOfPages)
